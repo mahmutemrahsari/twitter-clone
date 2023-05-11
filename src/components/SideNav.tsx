99bc026c62ba1093a66732fff12c9afe37327560
Mahmut Emrah Sari <mahmutemrahsari@gmail.com>
2023-05-11 21:30:08 +0200

    Profile

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export function SideNav(){
-    return <nav className="sticky top-0 px-2 py-4"></nav>
+    return (<nav className="sticky top-0 px-2 py-4">
+<ul className="flex flex-col items-start gap-2 whitespace-nowrap">
+    <li>
+        <Link href="/">Home</Link>
+    </li>
+    <li>
+        <Link href="/">Profile</Link>
+    </li>
+</ul>
+    </nav>);
 }
