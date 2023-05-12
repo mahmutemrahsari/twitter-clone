ac6ada16230cf12ef4593ddb09ac3d376602732a
Mahmut Emrah Sari <mahmutemrahsari@gmail.com>
2023-05-12 23:02:42 +0200

    Profile Image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,9 +19,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <meta name="description" content="This is a Twitter clone" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="container mx-auto flex items-start">
+      <div className="container mx-auto flex items-start sm:pr-4">
         <SideNav />
-        <div className="flex min-h-screen grow border-x">
+        <div className="min-h-screen flex-grow border-x">
           <Component {...pageProps} />
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
-
-import { api } from "~/utils/api";
+import { NewTweetForm } from "~/components/NewTweetForm";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
-  return <h1>Hi</h1>
+  return (
+    <>
+      <header className="sticky top-0 z-10 border-b bg-white pt-2">
+        <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
+      </header>
+      <NewTweetForm />
+    </>
+  );
 };
 
 export default Home;
